2ea32d50933c0e14ea5572de8e398a393b072113
lucasvonhagen <lucas.d.hansson@hotmail.com>
2025-04-13 14:46:05 +0200

    `Added environment variables to .env file and updated config.py to load them`

--- config.py
+++ config.py
@@ -1,7 +1,18 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
+    
+    X_API_KEY = os.environ.get('X_API_KEY') or "TWITTER API KEY NOT SET"
+    X_BEARER_TOKEN = os.environ.get('X_BEARER_TOKEN') or "TWITTER BEARER TOKEN NOT SET"
+
+    
+
