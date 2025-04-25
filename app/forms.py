dc9d4dc23c582f24898ab5da86e20ab05aba82aa
lucasvonhagen <lucas.d.hansson@hotmail.com>
2025-04-25 20:55:57 +0200

    Small change

--- app/forms.py
+++ app/forms.py
@@ -2,5 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+#Obsolete
 class submitForm(FlaskForm):
     tweet = StringField('Tweet', validators=[DataRequired()])
