5ade7c102c7658665d173ede06ab2054a3bb999b
lucasvonhagen <lucas.d.hansson@hotmail.com>
2025-04-26 19:02:52 +0200

    Added comments

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+
+#Init DB and app
 app = Flask(__name__, template_folder='templates')
 app.config.from_object(Config)
 db = SQLAlchemy(app)
